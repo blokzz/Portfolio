82fc175380bf330c286d09b64be867f9aff47bc8
blokz <thebloxpl@gmail.com>
2024-08-18 14:37:44 +0200

    map and about me update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled , {keyframes} from "styled-components"
-
+import Map from './map';
 const blob = keyframes`
   0%, 100% {
     border-radius: 77% 47% 61% 49%;
@@ -26,6 +26,8 @@ const blob = keyframes`
 const Section= styled.div`
 height: 100vh;
 scroll-snap-align: center;
+display: flex;
+flex-direction: row;
 `
 const SocialLink = styled.a`
   position: relative;
@@ -65,21 +67,23 @@ const SocialLink = styled.a`
 
 `
 const SocialContainer = styled.ul`
-  display: flex;
+  display: flex;;
   align-items: center;
   gap: 20px;
-  margin-top: 20px;
+  padding: 40px;
 `
 const Title = styled.h1`
 position: relative;
 font-size: 50px;
 `
 const Container = styled.div`
+  position: relative;
+  justify-content:space-between;
   display: flex;
   flex-direction: column;
-  width: 100%;
+  width: 40%;
   height: 100%;
-  padding: 20px;
+
   gap: 50px;
 `
 const Icon = styled.img`
@@ -89,19 +93,28 @@ const Icon = styled.img`
 const Mail = styled.h2`
   
 `
+const ContactContainer = styled.div`
+display: flex;
+flex-direction: column;
+gap: 30px;
+  padding: 40px;
+`
 function Contact() {
   return (
     <>
     <Section>
     <Container>
-    <Title>Contact & Social medias</Title>
+    <ContactContainer>
+    <Title>Contact</Title>
     <Mail>E-mail: dinoargentino@gmail.</Mail>
+    </ContactContainer>
     <SocialContainer>
     <SocialLink className="linkedin" target='_blank' href="https://linkedin.com"><Icon src='./img/linkedin.png'></Icon></SocialLink>
     <SocialLink className="github" target='_blank' href="https://github.com/blokzz"><Icon src='./img/github.png'></Icon></SocialLink>
     <SocialLink className="leet" target='_blank' href="https://leetcode.com/u/blokz/"><Icon src='./img/leet.png'></Icon></SocialLink>
     </SocialContainer>
     </Container>
+    <Map/>
     </Section>
     </>
   )
